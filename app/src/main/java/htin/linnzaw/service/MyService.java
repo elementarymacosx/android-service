e8a6ad0c9d7ec2a9df0a8bef7a7aa6acb0ff23d7
Htin Linn Zaw <p-aung@isgm2.com>
2019-12-19 16:18:22 +0630

    MyService.java First

--- app/src/main/java/htin/linnzaw/service/MyService.java
+++ app/src/main/java/htin/linnzaw/service/MyService.java
@@ -2,15 +2,44 @@ package htin.linnzaw.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.widget.Toast;
 
-public class MyService extends Service {
-    public MyService() {
+public class MyService extends Service
+{
+    private MediaPlayer mediaplayer;
+    public MyService()
+    {
+
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
+    public IBinder onBind(Intent intent)
+    {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate()
+    {
+        Toast.makeText(this, "Service created", Toast.LENGTH_SHORT).show();
+        mediaplayer = MediaPlayer.create(this, R.raw.eventually);
+        mediaplayer.setLooping(false);
+    }
+
+    @Override
+    public void onStart(Intent intent, int startid)
+    {
+        Toast.makeText(this, "Service  Started", Toast.LENGTH_SHORT).show();
+        mediaplayer.start();
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        Toast.makeText(this, "Service stopped", Toast.LENGTH_SHORT).show();
+        mediaplayer.stop();
     }
 }
